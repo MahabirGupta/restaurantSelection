c0fd3c0951f5f8f737b892413790146cbab5f5c0
MahabirGupta <mahabirg81@gmail.com>
2023-10-24 11:50:58 +0800

    Create project using Spring Web dependency

---
+++ src/main/java/com/mahabir/restaurantSelection/RestaurantSelectionApplication.java
@@ -0,0 +1,13 @@
+package com.mahabir.restaurantSelection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantSelectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantSelectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mahabir/restaurantSelection/RestaurantSelectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahabir.restaurantSelection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantSelectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
